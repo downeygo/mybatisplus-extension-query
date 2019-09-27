868f5730d94987a9f7181feed3c4bfa380199f14
imlent196 <imlent@163.com>
2019-09-27 09:55:17 +0800

    1.优化代码，更简洁 2.规范命名

---
+++ src/test/java/com/imen/mybatisplusextension/MybatisplusExtensionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imen.mybatisplusextension;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisplusExtensionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
